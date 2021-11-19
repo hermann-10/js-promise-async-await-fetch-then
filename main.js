03aaf1867213ae096cde62f02a6a46f80fca05a1
Hermann Manuel <hermann.manuel@hotmail.com.com>
2021-11-19 10:27:17 -0800

    Creation of a promise

--- main.js
+++ main.js
@@ -1,15 +1,32 @@
-console.log('Coucou JS');
-
 const btnJob = document.getElementById('btn-job');
+const candidat = { isProgrammer:true, 
+                   isCool:true,
+                };
 
 btnJob.addEventListener('click', applyToJob);
 
 function applyToJob(){
     console.log('La décision est en cours...');
-    setTimeout(() => {
-        console.log('Vous avez le job');
-    }, 2000);
-    console.log(`Recruteur - pendant ce temps je recois d'autres candidats`) 
+    const result = startDecisionProcess();
+    console.log('Result', result);
+}
+
+function startDecisionProcess(){
+    return new Promise((resolve, reject) => {
+        console.log(`Recruteur - pendant ce temps je recois d'autres candidats`) 
+        setTimeout(() => {
+            if(candidat.isProgrammer && candidat.isCool){
+                console.log('avant resolve')
+                resolve(`Recruteur - Bienvenue dans notre entreprise !`)
+    
+            }else{
+                console.log('avant reject')
+                reject(`Recruteur - Malgré tous l'inrérêt de votre candidature, nous sommes au regret...blablabla`)
+            }
+        }, 2000);
+
+        
+    });
 }
 
 console.log('Après la fonction applyToJob');
